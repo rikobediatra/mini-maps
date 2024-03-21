023e2ba3caa75c468295f5307e49917722071b23
rikobediatra <rikobediatra@gmail.com>
2024-03-21 15:46:17 +0700

    fixing marker

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,6 +1,6 @@
-import 'leaflet/dist/leaflet.css'
+import "leaflet/dist/leaflet.css"
 import type { Place } from "../api/Place";
-import type { Map as LeafletMap} from 'leaflet';
+import { Icon, type Map as LeafletMap} from 'leaflet';
 import { useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
@@ -11,6 +11,13 @@ interface MapProps {
 export default function Maps({ place }: MapProps) {
     const mapRef = useRef<LeafletMap | null>(null);
 
+    const customIcon = new Icon(
+        {
+            iconUrl: "https://cdn-icons-png.flaticon.com/128/1483/1483336.png",
+            iconSize: [40, 40]
+        }
+    );
+
     useEffect(() => {
         if (mapRef.current && place) {
             mapRef.current.flyTo([place.latitude, place.longitude]);
@@ -20,13 +27,13 @@ export default function Maps({ place }: MapProps) {
     return( 
         <MapContainer
             ref={mapRef}
-            center={[40.7, -74]}
+            center={[-6.175247, 106.8270488]}
             zoom={12}
             scrollWheelZoom
             className='h-full'
         >
-            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
-            {place && <Marker position={[place.latitude, place.longitude]} />}
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            {place && <Marker position={[place.latitude, place.longitude]} icon={customIcon}/>}
         </MapContainer>
     );
 }
